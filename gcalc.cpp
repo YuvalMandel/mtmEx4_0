e805179db2022924c07c95ecda75ccf09a030ba0
Yuval <yuvalm@aes-connect.com>
2020-08-05 10:41:18 +0300

    Started working on parser

--- gcalc.cpp
+++ gcalc.cpp
@@ -36,25 +36,113 @@ std::ostream& operator<<(std::ostream &os, const Gcalc& gcalc){
 
 }
 
-bool handleCommand(string command){
+string Gcalc::removeSpacesFromSides(const std::string& command){
+
+    string current_string = command;
+
+    while(current_string[0] == ' '){
+        current_string.erase(0);
+    }
+
+    while(current_string.back() == ' '){
+        current_string.pop_back();
+    }
+
+    return current_string;
+
+}
+
+bool Gcalc::checkSpecialCommand(const std::string& command, const std::string&
+token){
+
+    if(command.substr(0,token.length()) == token){
+        string sub_command = this -> removeSpacesFromSides(
+                command.substr(
+                        token.length() + 1,command.length() - token.length()));
+
+        if(sub_command[0] == '(' && sub_command.back()){
+
+            return true;
+
+        }
+    }
 
     return false;
 
 }
 
+Graph returnGraphFromExpression(const std::string& command){
+
+    Graph g;
+
+    return g;
+
+}
+
+int Gcalc::handleCommand(const string& command){
+
+    // First, we will remove any spaces from the sides.
+    string shaved_command = this -> removeSpacesFromSides(command);
+
+    // We will check if the commands are either "who", "reset" or "quit".
+    if(shaved_command == "who"){
+        std::cout << this;
+        return 0;
+    } else if (shaved_command == "reset"){
+        this -> reset();
+        return 0;
+    } else if (shaved_command == "quit"){
+        return 1;
+    }
+
+    string expression_to_calc;
+
+    // We will check if the command is "print", "save", "load" with  round
+    // brackets. If it is, we will calc the expression inside the brackets,
+    // and use the special function on them.
+    if(checkSpecialCommand(shaved_command, "print")){
+
+        expression_to_calc = shaved_command.substr(
+                5, shaved_command.length() - 5);
+
+        std::cout << this -> returnGraphFromExpression(expression_to_calc);
+
+        return 0;
+
+    } else if(checkSpecialCommand(shaved_command, "save")){
+
+        return 0;
+
+    } else if(checkSpecialCommand(shaved_command, "load")){
+
+        return 0;
+
+    }
+
+    // We will check if there is a single "="
+
+        // On the left side, we will check if it's valid graph name.
+
+        // On the right side, we will calc the expression to a graph.
+
+    // If all of them are false, this is an illegal command.
+
+}
+
 void prompt(){
 
-    bool exit = false;
+    int exit = 0;
     string command;
+    Gcalc gcalc;
 
     while(!exit){
 
         std::cout << "Gcalc> ";
         std::cin >> command;
 
-        exit = handleCommand(command);
+        exit = gcalc.handleCommand(command);
 
-        exit = true;
+        exit = 1;
 
     };
 
--- gcalc.h
+++ gcalc.h
@@ -14,6 +14,11 @@ public:
 
     void remove(const std::string& graph_name);
     void reset();
+    int handleCommand(const std::string& command);
+    bool checkSpecialCommand(const std::string& command,
+            const std::string& token);
+    static std::string removeSpacesFromSides(const std::string& command);
+    Graph returnGraphFromExpression(const std::string& command);
     Graph getGraph(const std::string& graph_name);
     friend std::ostream& operator<<(std::ostream &os, const Gcalc& gcalc);
 
