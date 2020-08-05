8eb09edc406271f5557e79a942e53858b1c43704
Yuval <yuvalm@aes-connect.com>
2020-08-05 16:49:27 +0300

    Can insert graph from string

--- gcalc.cpp
+++ gcalc.cpp
@@ -1,8 +1,12 @@
 #include "gcalc.h"
 #include <iterator>
+#include <cctype>
+#include <iostream>
 using std::endl;
 using std::string;
 using std::map;
+using std::exception;
+using std::getline;
 
 void Gcalc::remove(const std::string& graph_name){
 
@@ -26,7 +30,9 @@ Graph Gcalc::getGraph(const std::string& graph_name){
 
 std::ostream& operator<<(std::ostream &os, const Gcalc& gcalc){
 
-//    map<string, Graph>::iterator it = gcalc.graphs.begin();
+    if(gcalc.graphs.empty()){
+        return os;
+    }
 
     for(auto it = gcalc.graphs.begin(); it != gcalc.graphs.end(); ++it) {
         os << (it -> second) << endl;
@@ -41,7 +47,7 @@ string Gcalc::removeSpacesFromSides(const std::string& command){
     string current_string = command;
 
     while(current_string[0] == ' '){
-        current_string.erase(0);
+        current_string.erase(0, 1);
     }
 
     while(current_string.back() == ' '){
@@ -58,9 +64,9 @@ token){
     if(command.substr(0,token.length()) == token){
         string sub_command = this -> removeSpacesFromSides(
                 command.substr(
-                        token.length() + 1,command.length() - token.length()));
+                        token.length(),command.length() - token.length()));
 
-        if(sub_command[0] == '(' && sub_command.back()){
+        if(sub_command[0] == '(' && sub_command.back() == ')'){
 
             return true;
 
@@ -71,11 +77,152 @@ token){
 
 }
 
-Graph returnGraphFromExpression(const std::string& command){
+Edge Gcalc::creatEdgeFromString(const std::string& str){
+
+    Edge e;
+
+    if(str[0] == '<' && str.back() == '>'){
+
+        int location = str.find(',');
+
+        string leftSide = str.substr(1,location-1);
+        string rightSide = str.substr(location+1,str.length()-location-2);
+
+        leftSide = this -> removeSpacesFromSides(leftSide);
+        rightSide = this -> removeSpacesFromSides(rightSide);
+
+        e.first = leftSide;
+        e.second = rightSide;
+
+    }else{
+        throw BadEdge();
+    }
+
+    return e;
+
+}
+
+Graph Gcalc::creatGraphFromString(const std::string& exp){
+
+    Graph g;
+    int i = 1;
+    string current;
+
+    while(exp[i] != '|' && exp[i] != '}'){
+
+        if(exp[i] != ','){
+            current += exp[i];
+        }else{
+
+            current = this -> removeSpacesFromSides(current);
+
+            if(!current.empty()){
+                g.addVertex(current);
+                current = "";
+            }
+        }
+
+        ++i;
+
+    }
+
+    if(exp[i] == '|'){
+
+        current = this -> removeSpacesFromSides(current);
+
+        if(!current.empty()){
+            g.addVertex(current);
+            current = "";
+        }
+
+        i++;
+
+        while(exp[i] != '}'){
+
+//            current = this -> removeSpacesFromSides(current);
+            current += exp[i];
+
+            if(exp[i] == '>'){
+
+                current = this -> removeSpacesFromSides(current);
+
+                if(!current.empty()){
+                    Edge e = this -> creatEdgeFromString(current);
+                    g.addEdge(e);
+                    current = "";
+                }
+            }
+
+            ++i;
+
+        }
+    }
+
+    return g;
+
+}
+
+Graph Gcalc::returnGraphFromExpression(const std::string& exp){
 
     Graph g;
 
+    string shaved_exp = this -> removeSpacesFromSides(exp);
+
+    if(shaved_exp[0] == '(' && shaved_exp.back() == ')'){
+        shaved_exp.erase(0,1);
+        shaved_exp.pop_back();
+        g = this -> returnGraphFromExpression(shaved_exp);
+        return g;
+    }
+
+    if(shaved_exp[0] == '{' && shaved_exp.back() == '}'){
+        g = this -> creatGraphFromString(shaved_exp);
+        return g;
+    }
+
     return g;
+
+}
+
+int Gcalc::returnEqualsSignLocation(const std::string& command){
+
+    bool found = false;
+
+    int location = command.find('=');
+
+    if (location!=std::string::npos)
+        found = true;
+
+    int bad_location = command.substr(
+            location+1,command.length() - location).find('=');
+
+    if (bad_location!=std::string::npos || !found) {
+        return -1;
+    }
+
+    return location;
+
+}
+
+bool Gcalc::checkValidGraphName(const string& command){
+
+    if(!isalpha(command[0])){
+        return false;
+    }
+
+    for (int i = 0; i < command.length(); ++i) {
+        if(!isalnum(command[i])){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+void Gcalc::addGraph(const std::string& graph_name,const Graph& graph){
+
+    this -> graphs.insert(std::pair<string,Graph>(graph_name,graph));
 
 }
 
@@ -86,7 +233,7 @@ int Gcalc::handleCommand(const string& command){
 
     // We will check if the commands are either "who", "reset" or "quit".
     if(shaved_command == "who"){
-        std::cout << this;
+        std::cout << *this;
         return 0;
     } else if (shaved_command == "reset"){
         this -> reset();
@@ -120,12 +267,41 @@ int Gcalc::handleCommand(const string& command){
     }
 
     // We will check if there is a single "="
+    int equalsSignLocation = this -> returnEqualsSignLocation(shaved_command);
+
+    if(equalsSignLocation != -1){
 
         // On the left side, we will check if it's valid graph name.
+        string graph_name = this -> removeSpacesFromSides(
+                shaved_command.substr(0, equalsSignLocation));
+
+        if(!(this -> checkValidGraphName(graph_name))){
+            std::cout << "Error: Illegal command" << endl;
+            return 0;
+        }
 
         // On the right side, we will calc the expression to a graph.
+        string graph_expression = this -> removeSpacesFromSides(
+                shaved_command.substr(equalsSignLocation + 1,
+                        shaved_command.length() -equalsSignLocation - 1));
+
+        Graph graph;
+
+        try {
+            graph = this -> returnGraphFromExpression(graph_expression);
+        } catch(exception& e){
+            std::cout << "Error: " << e.what() << endl;
+            return 0;
+        }
+
+        this -> addGraph(graph_name, graph);
+        return 0;
+
+    }
 
     // If all of them are false, this is an illegal command.
+    std::cout << "Error: Illegal command" << endl;
+    return 0;
 
 }
 
@@ -137,12 +313,10 @@ void prompt(){
 
     while(!exit){
 
-        std::cout << "Gcalc> ";
-        std::cin >> command;
+        std::cout << "Gcalc>";
+        getline(std::cin,command);
 
         exit = gcalc.handleCommand(command);
-
-        exit = 1;
 
     };
 
@@ -157,7 +331,7 @@ int main(int argc, char *argv[]){
 
     } else if (argc == 3){
 
-
+        std::cout << argv[1] << " " << argv[2] << endl;
 
     }else{
 
--- gcalc.h
+++ gcalc.h
@@ -2,6 +2,7 @@
 #define MTMEX4_0_GCALC_H
 
 #include <iostream>
+#include <exception>
 #include <map>
 #include "graph.h"
 
@@ -18,9 +19,16 @@ public:
     bool checkSpecialCommand(const std::string& command,
             const std::string& token);
     static std::string removeSpacesFromSides(const std::string& command);
-    Graph returnGraphFromExpression(const std::string& command);
+    int returnEqualsSignLocation(const std::string& command);
+    Graph returnGraphFromExpression(const std::string& exp);
+    bool checkValidGraphName(const std::string& command);
     Graph getGraph(const std::string& graph_name);
+    Graph creatGraphFromString(const std::string& exp);
+    void addGraph(const std::string& graph_name,const Graph& graph);
+    Edge creatEdgeFromString(const std::string& str);
     friend std::ostream& operator<<(std::ostream &os, const Gcalc& gcalc);
+
+    class BadEdge: public std::exception{};
 
 };
 
