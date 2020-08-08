97b2f5e245cb0b5b457bed87e51f28be9e97ab3a
Yuval <yuvalm@aes-connect.com>
2020-08-08 20:16:32 +0300

    linux support () in name

--- graph.h
+++ graph.h
@@ -29,10 +29,6 @@ private:
 
 public:
 
-//    Graph() = default;
-//    ~Graph() = default;
-//    Graph& operator=(const Graph&) = default;
-
     bool checkEdgePossible(const Edge& e);
     bool checkVertexPossible(const Vertex& e);
     void addVertexToGraph(const Vertex& v);
